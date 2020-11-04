82d4622a2f96dfb5cfffa4af43af838dd6dfffe4
ThatcherK <kaliithatmau@gmail.com>
2020-11-04 14:13:11 +0300

    Add blog page

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('blog/', views.blog_page, name="blog_page")
      
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("here we are")
+    return render(request,'blogs/base.html',context=None)
+
+def blog_page(request):
+    return render(request, 'blogs/create_blog.html', context=None)
