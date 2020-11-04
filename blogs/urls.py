6f8362201ff58dd08662635789dae0f9a943d156
ThatcherK <kaliithatmau@gmail.com>
2020-11-04 22:12:10 +0300

    Add login view function

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('blog/', views.blog_page, name="blog_page"),
     path('signup_form/', views.signup_form, name="signup_form"),
-    path('user_signup/', views.user_signup, name="user_signup")
+    path('user_signup/', views.user_signup, name="user_signup"),
+    path('login_form/', views.login_form, name="login_form"),
+    path('user_login/', views.user_login, name="user_login"),
+    path ('user_logout/', views.logout, name="user_logout")
      
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .forms import SignupForm, LoginForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 def index(request):
-    return render(request,'blogs/base.html',context=None)
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    return render(request,'blogs/home.html',{'username': username})
 
 def blog_page(request):
     return render(request, 'blogs/create_blog.html', context=None)
@@ -19,10 +24,35 @@ def user_signup(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user_check = get_object_or_404(User, username=username)
-            if user_check is not None:
+            
+            try:
+                user_check = User.objects.get(username=username)
+                return HttpResponse("User already exists")
+            except User.DoesNotExist:
+
                 user = User.objects.create_user(username=username, password=password)
                 user.save()
-                return HttpResponse("Signup successful")
+                print(user)
+                return redirect(reverse('index'))
+
+def login_form(request):
+    form = LoginForm()
+    return render(request, 'blogs/login.html', context={'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('index'))
             else:
-                return HttpResponse("Username already exists")
+                return HttpResponse('Invalid log in')
+
+def user_logout(request):
+    logout(request)
+    return render(request,'blogs/home.html',context=None)
